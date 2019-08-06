ba3029ca62af6ee71a0d24e2cb0e874d68a7bd4d
Adrian <adrianivsdk@gmail.com>
2019-08-06 16:44:39 -0400

    Added Mumbling Kata

---
+++ mumbling-kata.js
@@ -0,0 +1,7 @@
+function accum(s) {
+	let letters = s.toLowerCase().split('')
+  for(let i = 0; i < letters.length; i++) {
+    letters[i] = letters[i].toUpperCase() + letters[i].repeat(i)
+  }
+  return letters.join('-')
+}
