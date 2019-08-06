b5d8c0d7d0716c389d530726117600383a81b9b9
Adrian <adrianivsdk@gmail.com>
2019-08-06 17:16:31 -0400

    Added Exes and Ohs Kata

---
+++ exes-and-ohs.js
@@ -0,0 +1,19 @@
+function XO(str) {
+    let oCount = 0
+    let xCount = 0
+    for(i=0; i < str.length; i++){
+    if (str.charAt(i).toLowerCase() == "x"){
+      xCount++;
+    } else if (str.charAt(i).toLowerCase() == "o"){
+      oCount++;
+    }
+    console.log(xCount, oCount)
+}
+    if (xCount === oCount){
+    return true;
+  } else if (xCount !== oCount){
+    return false;
+  } else if (xCount === 0 && oCount === 0){
+  return true
+  }
+}
